b198fda4548d1d65a6913549267e3170ed663315
Ammar <ammar.ejaz@bykea.com>
2019-10-12 18:10:11 +0500

    added the mongo db connection

--- my-own-fastify-practice/app.js
+++ my-own-fastify-practice/app.js
@@ -1,13 +1,16 @@
 const nconf = require('nconf');
 const server = require('./server');
 const { loadSetting } = require('./config/configurationAdapter');
+const { MongoConnect } = require('./api/mappers/db');
 
 const appSettingPath = process.env.APP_SETTINGS_FILE_PATH;
 
 
 loadSetting({ appSettingPath })
     .then(() =>{
-
+        nconf.set('db.mongodb.uri','mongodb://localhost/node_api');
+        const mongoURI = nconf.get('db.mongodb.uri');
+        MongoConnect.connectMongo(mongoURI);
         const serverOptions = {
             logSeverity : nconf.get('logSeverity'),
             
